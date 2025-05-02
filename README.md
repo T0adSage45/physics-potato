1bc14746494d48886db7d4661247ac051a040161
T0adSage45 <okami8542@gmail.com>
2025-05-02 17:50:46 +0900

    window rendered...

--- README.md
+++ README.md
@@ -5,4 +5,12 @@
 ```
  bear -- gcc -I/nix/store/pz6jgrfqyl2il2785a75vmwdp9vypv0j-glibc-2.40-66-dev/include -c -o main.o main.c
 ```
+```compile_json
+      "/nix/store/f0m6caffiykyvsjim9376a3hx2yj2ghj-gcc-wrapper-14.2.1.20250322/bin/gcc",
+      "-I/nix/store/pz6jgrfqyl2il2785a75vmwdp9vypv0j-glibc-2.40-66-dev/include",
+      "-I/nix/store/vjrb1ayf85l4b7xr21fi9kkdgxrk4fnc-sdl2-compat-2.32.52-dev/include",
+```|
+   |
+   --> **Specific to me , found your own self **
+
 choose nix files wisely...
--- src/main.c
+++ src/main.c
@@ -1,14 +1,14 @@
-#include "SDL2/SDL_platform.h"
-#include "SDL2/SDL_render.h"
-#include "SDL2/SDL_video.h"
-#include "wchar.h"
+// #include "SDL2/SDL_events.h"
+// #include "SDL2/SDL_keycode.h"
+// #include "SDL2/SDL_render.h"
+// #include "SDL2/SDL_video.h"
 #include <SDL2/SDL.h>
 #include <stdbool.h>
 #include <stdio.h>
 
 bool is_running = false;
-SDL_Window *window = NULL;
-SDL_Renderer *renderer = NULL;
+SDL_Window *window;
+SDL_Renderer *renderer;
 
 bool initalize_window(void) {
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -32,7 +32,42 @@ bool initalize_window(void) {
   return true;
 }
 
+void setup(void) {
+
+};
+void process_input(void) {
+  SDL_Event event;
+  SDL_PollEvent(&event);
+  switch (event.type) {
+  case SDL_QUIT:
+    is_running = false;
+    break;
+  case SDL_KEYDOWN:
+    if (event.key.keysym.sym == SDLK_ESCAPE) {
+      is_running = false;
+      break;
+    }
+  }
+};
+void update(void) {
+
+};
+void render(void) {
+  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+  SDL_RenderClear(renderer);
+
+  SDL_RenderPresent(renderer);
+};
+
 int main(void) {
   is_running = initalize_window();
+
+  setup();
+
+  while (is_running) {
+    process_input();
+    update();
+    render();
+  }
   return 0;
 }
