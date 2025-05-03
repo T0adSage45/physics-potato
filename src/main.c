d71da69a6bf0a6e4080242ba374645a0b13a39ca
T0adSage45 <okami8542@gmail.com>
2025-05-03 16:18:11 +0900

    added rect and grid..

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include "SDL2/SDL_keycode.h"
 #include "SDL2/SDL_video.h"
 #include <SDL2/SDL.h>
 #include <stdbool.h>
@@ -12,6 +13,7 @@ SDL_Texture *color_buffer_texture = NULL;
 uint32_t *color_buffer = NULL; // declaration of first element of memory.
 int window_width = 800;
 int window_height = 600;
+int grid_scale = 25;
 
 bool initalize_window(void) {
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -56,18 +58,34 @@ void setup(void) {
 
 void process_input(void) {
   SDL_Event event;
-  SDL_PollEvent(&event);
-  switch (event.type) {
-  case SDL_QUIT:
-    is_running = false;
-    break;
-  case SDL_KEYDOWN:
-    if (event.key.keysym.sym == SDLK_ESCAPE) {
+  while (SDL_PollEvent(&event)) {
+    switch (event.type) {
+    case SDL_QUIT:
       is_running = false;
+      break;
+
+    case SDL_KEYDOWN:
+      if (event.key.keysym.sym == SDLK_ESCAPE) {
+        is_running = false;
+      }
+
+      // Detect CTRL + PLUS
+      else if ((event.key.keysym.sym == SDLK_EQUALS ||
+                event.key.keysym.sym == SDLK_PLUS) &&
+               (event.key.keysym.mod & KMOD_CTRL)) {
+        grid_scale += 10;
+      }
+
+      // Detect CTRL + MINUS
+      else if (event.key.keysym.sym == SDLK_MINUS &&
+               (event.key.keysym.mod & KMOD_CTRL)) {
+        grid_scale -= 10;
+      }
+
       break;
     }
   }
-};
+}
 
 void update(void) {
 
@@ -88,12 +106,41 @@ void render_texture() {
   SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
 };
 
+void draw_grid() {
+  if (grid_scale < 10) {
+    grid_scale = 10;
+  }
+
+  if (grid_scale > 100) {
+    grid_scale = 100;
+  }
+
+  for (int y = 0; y < window_height; y += grid_scale) {
+    for (int x = 0; x < window_width; x += grid_scale) {
+
+      color_buffer[(window_width * y) + x] = 0x50333333;
+    }
+  }
+};
+
+void draw_rect(int x, int y, int width, int height, uint32_t color) {
+  for (int i = 0; i < width; i++) {
+    for (int j = 0; j < height; j++) {
+      int current_x = x + i;
+      int current_y = y + j;
+      color_buffer[(window_width * current_y) + current_x] = color;
+    }
+  }
+};
+
 void render(void) {
   SDL_SetRenderDrawColor(renderer, 0, 0, 25, 50);
   SDL_RenderClear(renderer);
 
+  draw_grid();
+  draw_rect(500, 200, 800, 400, 0xFFFF00FF);
   render_texture();
-  clear_color_buffer(0xFFFF0000);
+  clear_color_buffer(0xFF000000);
 
   SDL_RenderPresent(renderer);
 };
